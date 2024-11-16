200591137c3e26793e6d80d4d763feec845b583b
Bob3x <bob3xx@protonmail.com>
2024-11-16 18:26:54 +0200

    Fixing get token endpoint

--- src/api.js
+++ src/api.js
@@ -67,7 +67,7 @@ const getToken = async (code) => {
     try {
       const encodeCode = encodeURIComponent(code);
    
-      const response = await fetch( 'YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode);
+      const response = await fetch('https://7u8afzt0kl.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
