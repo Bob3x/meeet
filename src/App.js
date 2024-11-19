7d967a68f49aa58391315248d69b4995389ddaac
Bob3x <bob3xx@protonmail.com>
2024-11-19 18:15:12 +0200

    Chicking data props

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
   return (
     <div className="App">
       
-      <h1>Meet App</h1>
+      <h1>Meeet App</h1>
       <p>Find events in nearby cities</p>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
