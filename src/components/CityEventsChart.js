f30a6aca408e0d9c0584a3f8f6071641ec401581
Bob3x <bob3xx@protonmail.com>
2024-11-21 05:22:29 +0200

    Adjusting numbers displayed

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -30,9 +30,9 @@ const CityEventsChart = ({ allLocations, events }) => {
                 >
                 <CartesianGrid />
                 <XAxis type="category" dataKey="city" name="City" />
-                <YAxis type="number" dataKey="count" name="Number of events" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
+                <YAxis type="number" dataKey="count" name="Number of events" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} allowDecimals={false} />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-                <Scatter name="A school" data={data} fill="#8884d8" /> 
+                <Scatter name="City-events" data={data} fill="#8884d8" /> 
                 </ScatterChart>
         </ResponsiveContainer>
     );
