1fc715f5a2c01e873ee1c813cbbde6f134db6ef8
Bob3x <bob3xx@protonmail.com>
2024-11-22 03:28:37 +0200

    Adding more logs and fixing typo

--- src/api.js
+++ src/api.js
@@ -46,20 +46,19 @@ export const getAccessToken = async () => {
       console.log("Auth code from URL:", code);
 
       if (!code) {
-        console.log("No code, requesting auth URL");
+
         const response = await fetch(
           "https://7u8afzt0kl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
         )
-        .then(res => res.json())
+        .then(response => response.json())
         .then(console.log)
         .catch(console.error);
+
         const result = await response.json();
         console.log("Got auth URL:", result);
         window.location.href = result.authUrl;
         return null;
       }
-      
-      console.log("Has code, getting token");
       return getToken(code);
     }
 
@@ -83,16 +82,15 @@ const getToken = async (code) => {
     const encodedCode = encodeURIComponent(code);
     const response = await fetch(
       `https://7u8afzt0kl.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodedCode}`
-    )
-    .then(res => res.json())
-    .then(console.log)
-    .catch(console.error);
+    );
+    console.log("Token response:", response);
     
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     
     const json = await response.json();
+    console.log("Token data:", json);
     localStorage.setItem("access_token", json.access_token);
     return json.access_token;
   } catch (error) {
